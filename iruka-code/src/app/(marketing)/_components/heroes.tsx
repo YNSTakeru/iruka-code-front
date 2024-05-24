34d67fe09b0d12a694050c9ed47c30a37944acef
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-24 15:18:39 +0900

    Heroesを追加

---
+++ iruka-code/src/app/(marketing)/_components/heroes.tsx
@@ -0,0 +1,26 @@
+import Image from 'next/image';
+
+export const Heroes = () => {
+  return (
+    <div className="flex flex-col items-center justify-center max-w-5xl">
+      <div className="flex items-center gap-4">
+        <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
+          <Image
+            src="/documents.jpg"
+            fill
+            className="object-fill rounded-full md:rounded-lg"
+            alt="Documents"
+          />
+        </div>
+        <div className="relative h-[400px] w-[400px] hidden md:block">
+          <Image
+            src="/reading.jpg"
+            fill
+            className="object-fill rounded-lg"
+            alt="Reading"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
