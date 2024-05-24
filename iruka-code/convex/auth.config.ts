7060fd9c0d47ebf352e513799fbaaeba37f11fc2
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-25 00:20:49 +0900

    clerkを使って認証機能を実装

---
+++ iruka-code/convex/auth.config.ts
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: 'https://composed-caiman-80.clerk.accounts.dev',
+      applicationID: 'convex',
+    },
+  ],
+};
--- iruka-code/src/app/(marketing)/_components/navbar.tsx
+++ iruka-code/src/app/(marketing)/_components/navbar.tsx
@@ -1,11 +1,17 @@
 'use client';
 
 import { ModeToggle } from '@/components/mode-toggle';
+import { Spinner } from '@/components/spinner';
+import { Button } from '@/components/ui/button';
 import { useScrollTop } from '@/hooks/use-scroll-top';
 import { cn } from '@/lib/utils';
+import { SignInButton, UserButton } from '@clerk/clerk-react';
+import { useConvexAuth } from 'convex/react';
+import Link from 'next/link';
 import { Logo } from './logo';
 
 export const Navbar = () => {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
 
   return (
@@ -17,6 +23,27 @@ export const Navbar = () => {
     >
       <Logo />
       <div className="md:ml-auto md:justify-end justify-between w-full flex items-center gap-x-2">
+        {isLoading && <Spinner />}
+        {!isAuthenticated && !isLoading && (
+          <>
+            <SignInButton>
+              <Button variant="ghost" size="sm">
+                ログイン
+              </Button>
+            </SignInButton>
+            <SignInButton>
+              <Button size="sm">始める</Button>
+            </SignInButton>
+          </>
+        )}
+        {isAuthenticated && !isLoading && (
+          <>
+            <Button variant="ghost" size="sm" asChild>
+              <Link href="/documents">始める</Link>
+            </Button>
+            <UserButton afterSignOutUrl="/" />
+          </>
+        )}
         <ModeToggle />
       </div>
     </div>
