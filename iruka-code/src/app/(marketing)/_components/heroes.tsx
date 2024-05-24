837c6986993e45e4b1333fda27485d346d114507
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-24 15:48:24 +0900

    Footerと仮のナビゲーションを追加

--- iruka-code/src/app/(marketing)/_components/heroes.tsx
+++ iruka-code/src/app/(marketing)/_components/heroes.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export const Heroes = () => {
   return (
     <div className="flex flex-col items-center justify-center max-w-5xl">
-      <div className="flex items-center gap-4">
+      <div className="flex items-center gap-x-4">
         <div className="relative w-[300px] h-[300px] sm:w-[350px] sm:h-[350px] md:h-[400px] md:w-[400px]">
           <Image
             src="/documents.jpg"
