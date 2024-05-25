47c9a5fb62e799a85e39565592dda60662b8a35f
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-26 02:02:55 +0900

    プロジェクトが空の時に画像を表示する、また、ログインしたユーザーネームを表示する

--- iruka-code/src/app/(main)/(routes)/projects/page.tsx
+++ iruka-code/src/app/(main)/(routes)/projects/page.tsx
@@ -1,5 +1,35 @@
+'use client';
+
+import { useUser } from '@clerk/clerk-react';
+import Image from 'next/image';
+import { redirect } from 'next/navigation';
+
 const ProjectsPage = () => {
-  return <div>Project作成画面</div>;
+  const { user } = useUser();
+
+  if (!user) return redirect('/');
+
+  const name = user.firstName ? user.firstName : user.username;
+
+  return (
+    <div className="h-full flex flex-col items-center justify-center space-y-4">
+      <Image
+        src="/empty.jpg"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="dark:hidden border rounded-full h-[300px] w-[300px]"
+      />
+      <Image
+        src="/empty-dark.jpg"
+        height="300"
+        width="300"
+        alt="Empty"
+        className="hidden dark:block border rounded-full h-[300px] w-[300px]"
+      />
+      <h2>{name}さん、Iruka Codeへようこそ！</h2>
+    </div>
+  );
 };
 
 export default ProjectsPage;
