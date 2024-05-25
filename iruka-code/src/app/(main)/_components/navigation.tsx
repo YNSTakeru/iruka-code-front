4b9b6a3f68a0708fa1f47b31c8104d54bd75cdf9
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-26 02:12:53 +0900

    サイドバーのユーザー項目コンポーネントの骨格を追加

--- iruka-code/src/app/(main)/_components/navigation.tsx
+++ iruka-code/src/app/(main)/_components/navigation.tsx
@@ -5,6 +5,7 @@ import { ChevronsLeft, MenuIcon } from 'lucide-react';
 import { usePathname } from 'next/navigation';
 import { ElementRef, useEffect, useRef, useState } from 'react';
 import { useMediaQuery } from 'usehooks-ts';
+import { UserItem } from './user-item';
 
 export const Navigation = () => {
   const pathname = usePathname();
@@ -116,6 +117,9 @@ export const Navigation = () => {
           )}
         >
           <ChevronsLeft className="h-6 w-6" />
+          <div>
+            <UserItem />
+          </div>
         </div>
         <div>
           <p>Action items</p>
