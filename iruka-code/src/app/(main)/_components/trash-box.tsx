3531edfdf8daf78b14821f68fe564eecf980af1b
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-30 01:16:35 +0900

    ゴミ箱のチームを復元するアイコンを追加

--- iruka-code/src/app/(main)/_components/trash-box.tsx
+++ iruka-code/src/app/(main)/_components/trash-box.tsx
@@ -5,7 +5,7 @@ import { Input } from '@/components/ui/input';
 import { api } from '@convex/_generated/api';
 import { Id } from '@convex/_generated/dataModel';
 import { useMutation, useQuery } from 'convex/react';
-import { Search } from 'lucide-react';
+import { Search, Undo } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { toast } from 'sonner';
@@ -89,7 +89,16 @@ export const TrashBox = () => {
             onClick={() => onClick(team._id)}
             className="text-sm rounded-sm w-full hover:bg-primary/5 flex items-center text-primary justify-between"
           >
-            <span>{team.title}</span>
+            <span className="truncate pl-2">{team.title}</span>
+            <div className="flex items-center">
+              <div
+                onClick={(e) => onRestore(e, team._id)}
+                role="button"
+                className="rounded-sm p-2 hover:bg-neutral-200"
+              >
+                <Undo />
+              </div>
+            </div>
           </div>
         ))}
       </div>
