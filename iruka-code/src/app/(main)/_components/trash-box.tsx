c0bf21bc1043d23ee7125a1c2628148f6529f918
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-05-30 01:21:29 +0900

    ゴミ箱ポップアップにゴミ箱アイコンを追加

--- iruka-code/src/app/(main)/_components/trash-box.tsx
+++ iruka-code/src/app/(main)/_components/trash-box.tsx
@@ -5,7 +5,7 @@ import { Input } from '@/components/ui/input';
 import { api } from '@convex/_generated/api';
 import { Id } from '@convex/_generated/dataModel';
 import { useMutation, useQuery } from 'convex/react';
-import { Search, Undo } from 'lucide-react';
+import { Search, Trash, Undo } from 'lucide-react';
 import { useParams, useRouter } from 'next/navigation';
 import { useState } from 'react';
 import { toast } from 'sonner';
@@ -96,7 +96,13 @@ export const TrashBox = () => {
                 role="button"
                 className="rounded-sm p-2 hover:bg-neutral-200"
               >
-                <Undo />
+                <Undo className="h-4 w-4 text-muted-foreground" />
+              </div>
+              <div
+                role="button"
+                className="rounded-sm p-2 hover:bg-neutral-200"
+              >
+                <Trash className="h-4 w-4 text-muted-foreground" />
               </div>
             </div>
           </div>
