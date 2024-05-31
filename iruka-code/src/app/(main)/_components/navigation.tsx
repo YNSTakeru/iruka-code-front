5645ab361ac3e25309d5731d4addd0e2ce513169
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-01 03:09:04 +0900

    teamsページでライトモードとダークモードが切り替えられるように変更

--- iruka-code/src/app/(main)/_components/navigation.tsx
+++ iruka-code/src/app/(main)/_components/navigation.tsx
@@ -6,6 +6,7 @@ import {
   PopoverTrigger,
 } from '@/components/ui/popover';
 import { useSearch } from '@/hooks/use-search';
+import { useSettings } from '@/hooks/use-settings';
 import { cn } from '@/lib/utils';
 import { api } from '@convex/_generated/api';
 import { useMutation } from 'convex/react';
@@ -28,6 +29,7 @@ import { TrashBox } from './trash-box';
 import { UserItem } from './user-item';
 
 export const Navigation = () => {
+  const settings = useSettings();
   const search = useSearch();
   const pathname = usePathname();
   const isMobile = useMediaQuery('(max-width: 768px)');
@@ -153,7 +155,7 @@ export const Navigation = () => {
         <div>
           <UserItem />
           <Item label="検索" icon={Search} isSearch onClick={search.onOpen} />
-          <Item label="設定" icon={Settings} onClick={() => {}} />
+          <Item label="設定" icon={Settings} onClick={settings.onOpen} />
           <Item
             onClick={handleCreate}
             label="新規チーム作成"
---
+++ iruka-code/src/components/modals/settings-modal.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+import { useSettings } from '@/hooks/use-settings';
+import { ModeToggle } from '../mode-toggle';
+import { Dialog, DialogContent, DialogHeader } from '../ui/dialog';
+import { Label } from '../ui/label';
+
+export const SettingsModal = () => {
+  const settings = useSettings();
+
+  return (
+    <Dialog open={settings.isOpen} onOpenChange={settings.onClose}>
+      <DialogContent>
+        <DialogHeader className="border-b pb-3">
+          <h2 className="text-lg font-medium">ユーザー設定</h2>
+        </DialogHeader>
+        <div className="flex items-center justify-between">
+          <div className="flex flex-col gap-y-1">
+            <Label>モード切り替え</Label>
+            <span className="text-[0.8rem] text-muted-foreground">
+              ライトモード/ダークモードを切り替えます
+            </span>
+          </div>
+          <ModeToggle />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
+};
