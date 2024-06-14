0a83051ce2136f1c28daa97e97d41a96970c7fa6
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-14 13:18:21 +0900

    アイコンを追加するツールバーを追加

--- iruka-code/src/app/(main)/(routes)/teams/[teamId]/page.tsx
+++ iruka-code/src/app/(main)/(routes)/teams/[teamId]/page.tsx
@@ -1,5 +1,39 @@
-const TeamIdPage = () => {
-  return <div></div>;
+'use client';
+
+import { Toolbar } from '@/components/toolbar';
+import { api } from '@convex/_generated/api';
+import { Id } from '@convex/_generated/dataModel';
+import { useQuery } from 'convex/react';
+
+interface TeamIdPageProps {
+  params: {
+    teamId: Id<'teams'>;
+  };
+}
+
+const TeamIdPage = ({ params }: TeamIdPageProps) => {
+  const team = useQuery(api.teams.getById, {
+    teamId: params.teamId,
+  });
+
+  if (team === undefined) {
+    return <div>読み込み中...</div>;
+  }
+
+  if (team === null) {
+    return <div>見つかりません</div>;
+  }
+
+  console.log(params);
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:md-max-w-4xl mx-auto">
+        <Toolbar initialData={team} />
+      </div>
+    </div>
+  );
 };
 
 export default TeamIdPage;
