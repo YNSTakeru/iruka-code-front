fd032239d6d101b7758e1983755015ac04eaff61
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-14 13:21:23 +0900

    画像追加ボタンの追加

--- iruka-code/src/components/toolbar.tsx
+++ iruka-code/src/components/toolbar.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { Doc } from '@convex/_generated/dataModel';
-import { Smile, X } from 'lucide-react';
+import { ImageIcon, Smile, X } from 'lucide-react';
 import { IconPicker } from './icon-picker';
 import { Button } from './ui/button';
 
@@ -42,9 +42,19 @@ export const Toolbar = ({ initialData, preview }: ToolbarProps) => {
               size="sm"
             >
               <Smile className="h-4 w-4 mr-2" />
-              Add icon
+              アイコンを追加
             </Button>
           </IconPicker>
+        )}
+        {!initialData.coverImage && !preview && (
+          <Button
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+            onClick={() => {}}
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+          </Button>
         )}
       </div>
     </div>
