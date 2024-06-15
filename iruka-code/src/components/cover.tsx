7c21357fd0b9f7baefcf54efb7ffc32b5dea58dc
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-06-15 11:29:32 +0900

    画像の変更と削除ボタンの見た目を実装

--- iruka-code/src/components/cover.tsx
+++ iruka-code/src/components/cover.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import { cn } from '@/lib/utils';
+import { ImageIcon } from 'lucide-react';
 import Image from 'next/image';
+import { Button } from './ui/button';
 
 interface CoverImageProps {
   url?: string;
@@ -18,6 +20,28 @@ export const Cover = ({ url, preview }: CoverImageProps) => {
       )}
     >
       {!!url && <Image src={url} fill alt="Cover" className="object-cover" />};
+      {url && !preview && (
+        <div className="opacity-0 group-hover:opacity-100 absolute bottom-5 right-5 flex items-center gap-x-2">
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            画像を変更
+          </Button>
+          <Button
+            onClick={() => {}}
+            className="text-muted-foreground text-xs"
+            variant="outline"
+            size="sm"
+          >
+            <ImageIcon className="h-4 w-4 mr-2" />
+            画像を削除
+          </Button>
+        </div>
+      )}
     </div>
   );
 };
