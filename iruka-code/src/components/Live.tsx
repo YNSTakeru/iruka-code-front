4b3260575adfeaf1ddb7613d83a5bd9d7ed83769
YNSTakeru <95395710+YNSTakeru@users.noreply.github.com>
2024-08-09 22:12:19 +0900

    Toolbar & Shapes Menu

--- iruka-code/src/components/Live.tsx
+++ iruka-code/src/components/Live.tsx
@@ -12,7 +12,11 @@ import LiveCursors from './cursor/LiveCursors';
 import FlyingReaction from './reaction/FlyingReaction';
 import ReactionSelector from './reaction/ReactionButton';
 
-const Live = () => {
+type Props = {
+  canvasRef: React.MutableRefObject<HTMLCanvasElement | null>;
+};
+
+const Live = ({ canvasRef }: Props) => {
   const others = useOthers();
   const [{ cursor }, updateMyPresence] = useMyPresence() as any;
 
@@ -151,13 +155,14 @@ const Live = () => {
 
   return (
     <div
+      id="canvas"
       onPointerMove={handlePointerMove}
       onPointerLeave={handlePointerLeave}
       onPointerDown={handlePointerDown}
       onPointerUp={handlePointerUp}
       className="h-[100vh] w-full flex justify-center items-center text-center"
     >
-      <h1 className="text-2xl text-white">Liveblocks Figma Clone</h1>
+      <canvas ref={canvasRef} />
 
       {reaction.map((r) => (
         <FlyingReaction
